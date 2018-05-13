f2f468243733b780bfd50813f8819e962d8119ad
Jos Chong <Jos@jos-laptop.attlocal.net>
2018-05-13 14:03:24 -0500

    added standalone version

---
+++ Unity Files/Assets/Code/ShockTowerController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockTowerController : MonoBehaviour
+{
+	public GridController.Index Position;
+	private Transform _cap;
+	private float _lastTime, _waitTime, _shootTime;
+	private int _damage;
+	private LineRenderer[] _lineRenderers = new LineRenderer[3];
+
+	internal void Start()
+	{
+		_cap = gameObject.transform.GetChild(1);
+		_lastTime = 0f;
+		_waitTime = 3f;
+		_shootTime = 0.1f;
+		_damage = 25;
+
+		_lineRenderers[0] = GetComponent<LineRenderer>();
+		_lineRenderers[1] = gameObject.transform.GetChild(0).GetComponent<LineRenderer>();
+		_lineRenderers[2] = _cap.GetComponent<LineRenderer>();
+	}
+
+	internal void Update()
+	{
+		var closestEnemies = FindClosestEnemies();
+		if (closestEnemies.Count == 0)
+		{
+			ClearLines(0);
+			return;
+		}
+		
+		Vector3 direction = closestEnemies[0].transform.position - _cap.position;
+		Quaternion rotation = Quaternion.LookRotation(direction);
+		_cap.rotation = rotation;
+
+		if (Time.time < _lastTime + _waitTime) return;
+		StartCoroutine(Shock(closestEnemies));
+	}
+
+	private IEnumerator Shock(List<EnemyController> closestEnemies)
+	{
+		var enemies = closestEnemies;
+		_lastTime = Time.time;
+
+		while (Time.time < _lastTime + _shootTime)
+		{
+			var i = 0;
+			var lastPosition = _cap.position;
+			foreach (var enemy in enemies)
+			{
+				if (enemy != null)
+				{
+					_lineRenderers[i].SetPosition(0, lastPosition);
+					_lineRenderers[i++].SetPosition(1, enemy.gameObject.transform.position);
+					lastPosition = enemy.gameObject.transform.position;
+				}
+			}
+			if (i < 3)
+			{
+				ClearLines(i);
+			}
+			yield return null;
+		}
+		
+		foreach (var enemy in enemies)
+		{
+			if (enemy != null)
+			{
+				enemy.gameObject.GetComponent<EnemyController>().DealDamage(_damage);
+			}
+		}
+		ClearLines(0);
+	}
+
+	private void ClearLines(int i)
+	{
+		for (var x = i; x < 3; x++)
+		{
+			_lineRenderers[x].SetPosition(0, Vector3.zero);
+			_lineRenderers[x].SetPosition(1, Vector3.zero);
+		}
+	}
+	
+	private List<EnemyController> FindClosestEnemies()
+	{
+		var enemies = new List<EnemyController>();
+		
+		for (var i = 0; i < 3; i++)
+		{
+			if (i == 0 || enemies.Count == 0)
+			{
+				if (FindClosestEnemy(_cap, enemies) != null)
+				{
+					enemies.Add(FindClosestEnemy(_cap, enemies));
+				}
+			}
+			else
+			{
+				if (FindClosestEnemy(enemies[enemies.Count-1].gameObject.transform, enemies) != null)
+				{
+					enemies.Add(FindClosestEnemy(enemies[enemies.Count-1].gameObject.transform, enemies));
+				}
+			}
+		}
+
+		return enemies;
+	}
+
+	private EnemyController FindClosestEnemy(Transform go, List<EnemyController> targetedEnemies)
+	{
+		var enemies = FindObjectsOfType<EnemyController>();
+		EnemyController closestEnemy = null;
+		Vector3 pos = go.position;
+		foreach (var enemy in enemies)
+		{
+			if (go.gameObject.GetComponent<EnemyController>() != enemy && !targetedEnemies.Contains(enemy))
+			{
+				if (closestEnemy == null)
+				{
+					if (Vector3.Distance(pos, enemy.gameObject.transform.position) < 2)
+					{
+						closestEnemy = enemy;
+					}
+				}
+				else if (Vector3.Distance(pos, enemy.transform.position) <=
+				    Vector3.Distance(pos, closestEnemy.transform.position))
+				{
+					closestEnemy = enemy;
+				}
+			}
+		}
+		return closestEnemy;
+	}
+	
+	void OnMouseDown()
+	{
+		transform.parent.gameObject.GetComponent<CubeController>().ClickResponse();
+	}
+}
---
+++ Unity Files/Assets/Code/WaveControllerCopy.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveControllerCopy : MonoBehaviour
+{
+	private float _phaseStart;
+	private int _phaseState = 0; // 0 is break phase, 1 is wave phase
+	private float[] _phaseDuration = {10f, 40f}; // [0] is break duration, [1] is wave duration
+	private int _waveNumber = 1;
+	private Wave _nextWave;
+	private SpawnerController _spawner;
+
+	private int _wavesToWin = 5;
+	private Wave _nextNextWave;
+
+	private class Wave
+	{
+		public int[] MiniWaves = new int[6];
+		
+		public Wave(int waveNumber)
+		{
+			MiniWaves[0] = MiniWaves[2] = MiniWaves[4] = waveNumber + 4;
+			MiniWaves[1] = MiniWaves[5] = (waveNumber / 2) * 2;
+			MiniWaves[3] = waveNumber;
+		}
+	}
+	
+	internal void Start ()
+	{
+		_phaseStart = Time.time;
+		_nextWave = new Wave(_waveNumber);
+		_spawner = GetComponent<SpawnerController>();
+		FindObjectOfType<WaveInformationController>().Initialize();
+		
+		_nextNextWave = new Wave(_waveNumber + 1);
+	}
+	
+	internal void Update ()
+	{
+		if (Time.time < _phaseStart + _phaseDuration[_phaseState]) return;
+		
+		_phaseState = -_phaseState + 1;
+		if (_phaseState == 1) StartCoroutine(SendWave());
+		else if (_waveNumber > _wavesToWin && FindObjectsOfType<EnemyController>().Length == 0) FindObjectOfType<GridController>().EndGame("victory");
+		_phaseStart = Time.time;
+		
+		/*
+		if (_waveNumber > _wavesToWin && FindObjectsOfType<EnemyController>().Length == 0)
+		{
+			StopAllCoroutines();
+			if (FindObjectOfType<BaseController>().HP <= 0) return;
+			FindObjectOfType<GridController>().EndGame("victory");
+			return;
+		}
+		*/
+	}
+
+	private IEnumerator SendWave()
+	{
+		yield return StartCoroutine(SendMiniWave(0));
+		yield return StartCoroutine(SendMiniWave(1, "fast"));
+		yield return StartCoroutine(SendMiniWave(2));
+		yield return StartCoroutine(SendMiniWave(3, "strong"));
+		yield return StartCoroutine(SendMiniWave(4));
+		yield return StartCoroutine(SendMiniWave(5, "fast"));
+		_nextWave = new Wave(++_waveNumber);
+		
+		_nextNextWave = new Wave(_waveNumber + 1);
+	}
+
+	private IEnumerator SendMiniWave(int miniWave, string type = "normal")
+	{
+		for (var s = 0; s < 5; s++)
+		{
+			var delay = 0f;
+			for (var e = 0; e < _nextWave.MiniWaves[miniWave] / 5; e++)
+			{
+				_spawner.SpawnEnemy(type, _waveNumber);
+				yield return new WaitForSeconds(0.25f);
+				delay += 0.25f;
+			}
+			if (_nextWave.MiniWaves[miniWave] % 5 > 0)
+			{
+				_spawner.SpawnEnemy(type, _waveNumber);
+				_nextWave.MiniWaves[miniWave]--;
+			}
+			yield return new WaitForSeconds(1f - delay);
+		}
+	}
+
+	public int GetPhase()
+	{
+		return _phaseState;
+	}
+
+	public float GetPhaseStart()
+	{
+		return _phaseStart;
+	}
+
+	public float[] GetPhaseDurations()
+	{
+		return new float[] {_phaseDuration[0] + _phaseDuration[1], 2 * _phaseDuration[0] + _phaseDuration[1]};
+	}
+	
+	public int GetWaveNumber()
+	{
+		return _waveNumber;
+	}
+
+	public int[] GetMiniWaves()
+	{
+		return _nextWave.MiniWaves;
+	}
+	
+	public int GetWaveNumberNext()
+	{
+		return _waveNumber + 1;
+	}
+	
+	public int[] GetMiniWavesNext()
+	{
+		return _nextNextWave.MiniWaves;
+	}
+
+	public int GetWavesToWin()
+	{
+		return _wavesToWin;
+	}
+}
